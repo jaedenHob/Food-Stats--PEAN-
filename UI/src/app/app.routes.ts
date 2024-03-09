9ecf0a4910d8d87465f862e106e5d9e8046b1f9b
jaedenHob <jaedenhuxtable@gmail.com>
2024-03-09 18:42:19 -0500

    Creating routing for Login, register, and dashboard componenets.

--- UI/src/app/app.routes.ts
+++ UI/src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+// components to use
+import { LoginComponent } from './Components/login/login.component';
+import { RegisterComponent } from './Components/register/register.component';
+import { DashboardComponent } from './Components/dashboard/dashboard.component';
+
+
+export const routes: Routes = [
+    {
+        path: 'login', 
+        component: LoginComponent
+    },
+
+    { 
+        path: 'register', 
+        component: RegisterComponent 
+    },
+
+    { 
+        path: 'dashboard', 
+        component: DashboardComponent 
+    },
+    
+    {
+        path:'', redirectTo: 'dashboard', pathMatch: 'full'
+    }
+];
