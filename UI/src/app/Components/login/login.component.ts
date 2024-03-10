d00048ed1ed9375080b19fedccc2f390be045286
jaedenHob <jaedenhuxtable@gmail.com>
2024-03-09 19:54:07 -0500

    got primeng to work with angular standalone components although it was mainly used for angular's modular components

--- UI/src/app/Components/login/login.component.ts
+++ UI/src/app/Components/login/login.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CardModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
