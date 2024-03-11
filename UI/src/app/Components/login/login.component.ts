b83d3b97a24419ccb4b860ce798aeac25a221009
jaedenHob <jaedenhuxtable@gmail.com>
2024-03-11 19:17:29 -0400

    Created both the login page and register pages forms for user logging in.

--- UI/src/app/Components/login/login.component.ts
+++ UI/src/app/Components/login/login.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { RouterLink } from '@angular/router';
+import { RouterLinkActive } from '@angular/router';
+
 import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CardModule],
+  imports: [
+    CardModule, InputTextModule, ButtonModule, RouterOutlet, RouterLink, RouterLinkActive, CommonModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
--- UI/src/app/Components/register/register.component.ts
+++ UI/src/app/Components/register/register.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
 
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { RouterLink } from '@angular/router';
+import { RouterLinkActive } from '@angular/router';
+
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [
+    CardModule, InputTextModule, ButtonModule, RouterOutlet, RouterLink, RouterLinkActive, CommonModule
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
